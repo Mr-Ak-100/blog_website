7e805e006931cd97f106261e7c48eaff9990eb6f
Abolfazl Khorsand <Abolfazl-Kh@outlook.com>
2023-08-06 19:32:52 +0330

    adding_contact_and_about_page

--- home/views.py
+++ home/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home/index.html", {"sidebar": True})
+
+
+def about(request):
+    return render(request, "home/about.html", {"sidebar": True})
+
+
+def contact(request):
+    return render(request, "home/contact.html")
