63c3f9c0d2f1d6bba6c314ac4e57e8c83103c254
Abolfazl Khorsand <Abolfazl-Kh@outlook.com>
2023-08-09 20:39:42 +0330

    article_model

--- article/models.py
+++ article/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
 
     title = models.CharField(max_length=35, unique=True)
     slug = models.SlugField(max_length=30, verbose_name="Display Name", primary_key=True)
-    sidebar = models.BooleanField(default=False,)
+    sidebar = models.BooleanField(default=False, help_text="show in sidebar")
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -14,4 +15,25 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
+    category = models.ManyToManyField(Category)
+    slug = models.SlugField(max_length=30, verbose_name="Display Name", primary_key=True)
+    main_image = models.ImageField(upload_to="articles_images/")
+    body = models.TextField()
+
+    views = models.IntegerField(default=0)
+    banner = models.BooleanField(default=False, verbose_name="Home Banner", help_text="show in home banners")
+    home_item = models.BooleanField(default=False, help_text="show in home page articles")
+    published = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author.username} > {self.title[:40]}"
 
