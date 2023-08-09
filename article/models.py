f5d13353c49692108cd21f2d60c18415c2d13f74
Abolfazl Khorsand <Abolfazl-Kh@outlook.com>
2023-08-09 20:09:08 +0330

    category_model

--- article/models.py
+++ article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    title = models.CharField(max_length=35, unique=True)
+    slug = models.SlugField(max_length=30, verbose_name="Display Name", primary_key=True)
+    sidebar = models.BooleanField(default=False,)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-sidebar"]
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+
