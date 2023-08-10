b7293d18a64e3d734d30db0d78b71201670324b6
Abolfazl Khorsand <Abolfazl-Kh@outlook.com>
2023-08-10 18:49:49 +0330

    handel_invalid_articles_page

--- article/views.py
+++ article/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from . models import Article
 from django.core.paginator import Paginator
+from django.http import Http404
 
 
 def articles(request):
 
     banner_name = "پست ها"
 
-    page = request.GET.get("page")
-    if page:
-        int(page)
+    try:
+
+        page = request.GET.get("page")
+        if page:
+            int(page)
+
+    except ValueError:
+        raise Http404()
 
     paginator = Paginator(Article.objects.published(), 6)
     objects = paginator.get_page(page)
