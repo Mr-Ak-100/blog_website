c268530ba90fc57cb1f651e71ee8060f902a5639
Abolfazl Khorsand <Abolfazl-Kh@outlook.com>
2023-08-10 18:35:05 +0330

    apply_articles_page

--- article/views.py
+++ article/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from . models import Article
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def articles(request):
+
+    banner_name = "پست ها"
+
+    page = request.GET.get("page")
+    if page:
+        int(page)
+
+    paginator = Paginator(Article.objects.published(), 6)
+    objects = paginator.get_page(page)
+
+    return render(request, "article/articles.html", {"sidebar": True, "articles": objects, "banner_name": banner_name})
