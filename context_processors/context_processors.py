2a83a4afad0e640fafc576188b88a0aa2688cc6c
Abolfazl Khorsand <Abolfazl-Kh@outlook.com>
2023-08-14 23:15:17 +0330

    apply_sidebar_most_views

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,12 +1,13 @@
 from home.models import Info, Social
-from article.models import Category
+from article.models import Category, Article
 
 
 def sidebar(request):
 
     sidebar_categories = Category.objects.sidebar_items()
+    sidebar_most_view = Article.objects.published().order_by("-views")[:3]
 
-    return {"sidebar": False, "sidebar_categories": sidebar_categories}
+    return {"sidebar": False, "sidebar_categories": sidebar_categories, "sidebar_most_view": sidebar_most_view}
 
 
 def main_info(request):
