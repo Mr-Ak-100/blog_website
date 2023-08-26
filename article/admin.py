41252c50131f0e07d57e6b9cbd210512b976324c
Abolfazl Khorsand <Abolfazl-Kh@outlook.com>
2023-08-26 04:02:20 +0330

    config_summernote_for_about_and_articles

--- article/admin.py
+++ article/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from . models import Category, Article, ArticleView, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Category)
-admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    summernote_fields = ("body",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(ArticleView)
 admin.site.register(Comment)
--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from . models import Message, Info, Social, Log
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Info)
+class InfoAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ("main_text", "about_text")
+
 
 admin.site.register(Message)
-admin.site.register(Info)
 admin.site.register(Social)
 admin.site.register(Log)
